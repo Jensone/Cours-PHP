44ac3010eb9567d948bd52391ebd6c3c32baf019
Jensone <hello@jensone.com>
2023-05-04 16:06:12 +0200

    ✨ Affichage des article sur l'index.php

--- data.php
+++ data.php
@@ -20,17 +20,21 @@ function allPosts(){
         file_get_contents(__DIR__ . '/assets/articles.json'), true // true pour tableau associatif
     );
     foreach($articles as $valeur) { // Boucle sur les articles du tableau associatif : Test OK
-        echo '<div class="card col-md-3 col-sm-12">';
-        echo '<img src="https://placeholder.com/400" class="card-img-top mt-2 rounded" alt="CHANGE">';
-        echo '<div class="card-body">';
-        echo '<h5 class="card-title">CHANGE ICI</h5>';
-        echo '<p class="card-text fs-6">C</p>';
-        echo '<a href="#" class="btn btn-primary rounded-5"></a>';
+        echo '<div class="card col-md-3 col-sm-12">'; // Début de la carte
+        echo '<img src="https://placeholder.com/400" class="card-img-top mt-2 rounded" alt="' . $valeur['titre'] . '">';
+        echo '<div class="card-body">'; // Début du corps de la carte
+        echo '<h5 class="card-title">' . $valeur['titre'] . '</h5>'; // Titre de la carte
+        echo '<p class="card-text fs-6">' . $valeur['contenu'] . '</p>'; // Contenu de la carte
+        echo '<a href="#" class="btn btn-primary rounded-5">Lire la suite</a>'; // Bouton de la carte
+        echo '</div></div>'; // Fin du corps de la carte avec les 2 div imbriquées
     }
 }
 
 // TODO: Boucle sur les 3 derniers articles
 
+function lastPosts(){
+
+}
 
 
 // TODO: Affichage d'un article dans la page article.php
--- index.php
+++ index.php
@@ -20,15 +20,8 @@ include dirname(__FILE__) . '/partials/_aside.php';
                     <hr>
                     <h3 class="text-center">Lisez nos articles</h3>
                     <div class="row gap-3 justify-content-center">
-                        
-                        <div class="card col-md-3 col-sm-12">
-                            <img src="https://placeholder.com/400" class="card-img-top mt-2 rounded" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text fs-6">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary rounded-5">Go somewhere</a>
-                            </div>
-                        </div>
+
+                        <?php allPosts(); ?>
 
                     </div>
                 </div>
